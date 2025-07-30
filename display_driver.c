f4471cf0b5dcbbc5d02dcbaee4515f8cd6f6d285
Davide Bettio <davide@uninstall.it>
2025-07-30 19:11:47 +0200

    Add also "sitronix,st7796" as supported model
    Signed-off-by: Davide Bettio <davide@uninstall.it>

--- display_driver.c
+++ display_driver.c
@@ -65,7 +65,8 @@ Context *display_create_port(GlobalContext *global, term opts)
         ctx = ssd1306_display_create_port(global, opts);
     } else if (!strcmp(compat_string, "sino-wealth,sh1106")) {
         ctx = ssd1306_display_create_port(global, opts);
-    } else if (!strcmp(compat_string, "sitronix,st7789")) {
+    } else if (!strcmp(compat_string, "sitronix,st7789")
+        || !strcmp(compat_string, "sitronix,st7796")) {
         ctx = st7789_display_create_port(global, opts);
     } else {
         ESP_LOGE(TAG, "No matching display driver for given `comptaible`: `%s`.", compat_string);
