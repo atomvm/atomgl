23cccf3ee9d257eacb1b6191c63341319cdc461a
Ibrahim YILMAZ <ibrahim@drlinux.org>
2026-06-17 13:58:27 +0200

    dcs_lcd_draw: Add display-list compositing for transparent backgrounds
    When a primitive has transparent background (brcolor == 0), partial-alpha
    pixels were previously blended against the current framebuffer content,
    which is unreliable with double-buffered region rendering: the work
    framebuffer contains uninitialized PSRAM data rather than the intended
    lower display-list layer.
    
    Add dcs_lcd_resolve_pixel_rgb565() and per-primitive pixel resolution
    functions that walk the display list to find the solid colour from the
    next lower opaque item.  Partial-alpha transparent pixels now blend
    over that resolved lower-layer pixel instead of framebuffer memory.
    
    This enables correct anti-aliased uFont text rendering over transparent
    backgrounds on RGB LCD displays.
    
    Signed-off-by: Ibrahim YILMAZ <ibrahim@drlinux.org>

--- dcs_lcd_draw.c
+++ dcs_lcd_draw.c
@@ -46,8 +46,136 @@ int dcs_lcd_find_max_line_len(const struct DCSLCDScreen *screen,
     return line_len;
 }
 
+static bool dcs_lcd_resolve_pixel_rgb565(const struct DCSLCDScreen *screen,
+    int xpos, int ypos, BaseDisplayItem items[], size_t items_len, size_t start_index, uint16_t *out_color);
+
+static bool dcs_lcd_image_pixel_rgb565(const struct DCSLCDScreen *screen,
+    BaseDisplayItem *item, int xpos, int ypos, BaseDisplayItem items[], size_t items_len, size_t item_index, uint16_t *out_color)
+{
+    int x = item->x;
+    int y = item->y;
+    int rel_x = xpos - x;
+    int rel_y = ypos - y;
+    uint32_t *pixels = ((uint32_t *) item->data.image_data.pix) + (rel_y * item->width) + rel_x;
+    uint32_t img_pixel = READ_32_UNALIGNED(pixels);
+    uint8_t alpha = rgba8888_get_alpha(img_pixel);
+
+    if (alpha == 0xFF) {
+        *out_color = rgba8888_color_to_rgb565(img_pixel);
+        return true;
+    }
+    if (item->brcolor != 0) {
+        uint16_t color = rgba8888_color_to_rgb565(img_pixel);
+        uint16_t bgcolor = display_color_to_rgb565(item->brcolor);
+        *out_color = alpha_blend_rgb565(color, bgcolor, alpha);
+        return true;
+    }
+    if (alpha > 0) {
+        uint16_t lower = 0;
+        uint16_t color = rgba8888_color_to_rgb565(img_pixel);
+        (void) dcs_lcd_resolve_pixel_rgb565(screen, xpos, ypos, items, items_len, item_index + 1, &lower);
+        *out_color = alpha_blend_rgb565(color, lower, alpha);
+        return true;
+    }
+
+    return dcs_lcd_resolve_pixel_rgb565(screen, xpos, ypos, items, items_len, item_index + 1, out_color);
+}
+
+static bool dcs_lcd_scaled_image_pixel_rgb565(const struct DCSLCDScreen *screen,
+    BaseDisplayItem *item, int xpos, int ypos, BaseDisplayItem items[], size_t items_len, size_t item_index, uint16_t *out_color)
+{
+    int x = item->x;
+    int y = item->y;
+    int source_x = item->source_x + ((xpos - x) / item->x_scale);
+    int source_y = item->source_y + ((ypos - y) / item->y_scale);
+    int img_width = item->data.image_data_with_size.width;
+    uint32_t *pixels = ((uint32_t *) item->data.image_data_with_size.pix) + (source_y * img_width) + source_x;
+    uint32_t img_pixel = READ_32_UNALIGNED(pixels);
+    uint8_t alpha = rgba8888_get_alpha(img_pixel);
+
+    if (alpha == 0xFF) {
+        *out_color = rgba8888_color_to_rgb565(img_pixel);
+        return true;
+    }
+    if (item->brcolor != 0) {
+        uint16_t color = rgba8888_color_to_rgb565(img_pixel);
+        uint16_t bgcolor = display_color_to_rgb565(item->brcolor);
+        *out_color = alpha_blend_rgb565(color, bgcolor, alpha);
+        return true;
+    }
+    if (alpha > 0) {
+        uint16_t lower = 0;
+        uint16_t color = rgba8888_color_to_rgb565(img_pixel);
+        (void) dcs_lcd_resolve_pixel_rgb565(screen, xpos, ypos, items, items_len, item_index + 1, &lower);
+        *out_color = alpha_blend_rgb565(color, lower, alpha);
+        return true;
+    }
+
+    return dcs_lcd_resolve_pixel_rgb565(screen, xpos, ypos, items, items_len, item_index + 1, out_color);
+}
+
+static bool dcs_lcd_text_pixel_rgb565(const struct DCSLCDScreen *screen,
+    BaseDisplayItem *item, int xpos, int ypos, BaseDisplayItem items[], size_t items_len, size_t item_index, uint16_t *out_color)
+{
+    int x = item->x;
+    int y = item->y;
+    char *text = (char *) item->data.text_data.text;
+    int char_index = (xpos - x) / CHAR_WIDTH;
+    char c = text[char_index];
+    unsigned const char *glyph = fontdata + ((unsigned char) c) * 16;
+    unsigned char row = glyph[ypos - y];
+    int k = (xpos - x) % CHAR_WIDTH;
+
+    if (row & (1 << (7 - k))) {
+        *out_color = display_color_to_rgb565(item->data.text_data.fgcolor);
+        return true;
+    }
+    if (item->brcolor != 0) {
+        *out_color = display_color_to_rgb565(item->brcolor);
+        return true;
+    }
+
+    return dcs_lcd_resolve_pixel_rgb565(screen, xpos, ypos, items, items_len, item_index + 1, out_color);
+}
+
+static bool dcs_lcd_resolve_pixel_rgb565(const struct DCSLCDScreen *screen,
+    int xpos, int ypos, BaseDisplayItem items[], size_t items_len, size_t start_index, uint16_t *out_color)
+{
+    for (size_t i = start_index; i < items_len; i++) {
+        BaseDisplayItem *item = &items[i];
+        if ((xpos < item->x) || (xpos >= item->x + item->width) || (ypos < item->y) || (ypos >= item->y + item->height)) {
+            continue;
+        }
+
+        switch (item->primitive) {
+            case PrimitiveImage:
+                if (dcs_lcd_image_pixel_rgb565(screen, item, xpos, ypos, items, items_len, i, out_color)) {
+                    return true;
+                }
+                break;
+            case PrimitiveRect:
+                *out_color = display_color_to_rgb565(item->brcolor);
+                return true;
+            case PrimitiveScaledCroppedImage:
+                if (dcs_lcd_scaled_image_pixel_rgb565(screen, item, xpos, ypos, items, items_len, i, out_color)) {
+                    return true;
+                }
+                break;
+            case PrimitiveText:
+                if (dcs_lcd_text_pixel_rgb565(screen, item, xpos, ypos, items, items_len, i, out_color)) {
+                    return true;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    return false;
+}
+
 int dcs_lcd_draw_image_x(const struct DCSLCDScreen *screen,
-    int xpos, int ypos, int max_line_len, BaseDisplayItem *item)
+    int xpos, int ypos, int max_line_len, BaseDisplayItem *item,
+    BaseDisplayItem items[], size_t items_len, size_t item_index)
 {
     int x = item->x;
     int y = item->y;
@@ -83,6 +211,12 @@ int dcs_lcd_draw_image_x(const struct DCSLCDScreen *screen,
             uint16_t color = rgba8888_color_to_rgb565(img_pixel);
             uint16_t blended = alpha_blend_rgb565(color, bgcolor, alpha);
             pixmem16[drawn_pixels] = rgb565_color_to_surface(blended);
+        } else if (alpha > 0) {
+            uint16_t color = rgba8888_color_to_rgb565(img_pixel);
+            uint16_t lower = 0;
+            (void) dcs_lcd_resolve_pixel_rgb565(screen, xpos + drawn_pixels, ypos, items, items_len, item_index + 1, &lower);
+            uint16_t blended = alpha_blend_rgb565(color, lower, alpha);
+            pixmem16[drawn_pixels] = rgb565_color_to_surface(blended);
         } else {
             return drawn_pixels;
         }
@@ -117,7 +251,8 @@ int dcs_lcd_draw_rect_x(const struct DCSLCDScreen *screen,
 }
 
 int dcs_lcd_draw_text_x(const struct DCSLCDScreen *screen,
-    int xpos, int ypos, int max_line_len, BaseDisplayItem *item)
+    int xpos, int ypos, int max_line_len, BaseDisplayItem *item,
+    BaseDisplayItem items[], size_t items_len, size_t item_index)
 {
     int x = item->x;
     int y = item->y;
@@ -163,7 +298,11 @@ int dcs_lcd_draw_text_x(const struct DCSLCDScreen *screen,
         } else if (visible_bg) {
             pixmem16[drawn_pixels] = bgcolor;
         } else {
-            return drawn_pixels;
+            uint16_t lower = 0;
+            if (!dcs_lcd_resolve_pixel_rgb565(screen, xpos + drawn_pixels, ypos, items, items_len, item_index + 1, &lower)) {
+                return drawn_pixels;
+            }
+            pixmem16[drawn_pixels] = rgb565_color_to_surface(lower);
         }
         drawn_pixels++;
     }
@@ -172,7 +311,8 @@ int dcs_lcd_draw_text_x(const struct DCSLCDScreen *screen,
 }
 
 int dcs_lcd_draw_scaled_cropped_img_x(const struct DCSLCDScreen *screen,
-    int xpos, int ypos, int max_line_len, BaseDisplayItem *item)
+    int xpos, int ypos, int max_line_len, BaseDisplayItem *item,
+    BaseDisplayItem items[], size_t items_len, size_t item_index)
 {
     int x = item->x;
     int y = item->y;
@@ -219,6 +359,12 @@ int dcs_lcd_draw_scaled_cropped_img_x(const struct DCSLCDScreen *screen,
             uint16_t color = rgba8888_color_to_rgb565(img_pixel);
             uint16_t blended = alpha_blend_rgb565(color, bgcolor, alpha);
             pixmem16[drawn_pixels] = rgb565_color_to_surface(blended);
+        } else if (alpha > 0) {
+            uint16_t color = rgba8888_color_to_rgb565(img_pixel);
+            uint16_t lower = 0;
+            (void) dcs_lcd_resolve_pixel_rgb565(screen, xpos + drawn_pixels, ypos, items, items_len, item_index + 1, &lower);
+            uint16_t blended = alpha_blend_rgb565(color, lower, alpha);
+            pixmem16[drawn_pixels] = rgb565_color_to_surface(blended);
         } else {
             return drawn_pixels;
         }
@@ -245,7 +391,7 @@ int dcs_lcd_draw_x(const struct DCSLCDScreen *screen,
         int drawn_pixels = 0;
         switch (items[i].primitive) {
             case PrimitiveImage:
-                drawn_pixels = dcs_lcd_draw_image_x(screen, xpos, ypos, max_line_len, item);
+                drawn_pixels = dcs_lcd_draw_image_x(screen, xpos, ypos, max_line_len, item, items, items_len, i);
                 break;
 
             case PrimitiveRect:
@@ -253,11 +399,11 @@ int dcs_lcd_draw_x(const struct DCSLCDScreen *screen,
                 break;
 
             case PrimitiveScaledCroppedImage:
-                drawn_pixels = dcs_lcd_draw_scaled_cropped_img_x(screen, xpos, ypos, max_line_len, item);
+                drawn_pixels = dcs_lcd_draw_scaled_cropped_img_x(screen, xpos, ypos, max_line_len, item, items, items_len, i);
                 break;
 
             case PrimitiveText:
-                drawn_pixels = dcs_lcd_draw_text_x(screen, xpos, ypos, max_line_len, item);
+                drawn_pixels = dcs_lcd_draw_text_x(screen, xpos, ypos, max_line_len, item, items, items_len, i);
                 break;
             default: {
                 fprintf(stderr, "unexpected display list command.\n");
--- dcs_lcd_draw.h
+++ dcs_lcd_draw.h
@@ -25,16 +25,19 @@
 #include "display_items.h"
 
 int dcs_lcd_draw_image_x(const struct DCSLCDScreen *screen,
-    int xpos, int ypos, int max_line_len, BaseDisplayItem *item);
+    int xpos, int ypos, int max_line_len, BaseDisplayItem *item,
+    BaseDisplayItem items[], size_t items_len, size_t item_index);
 
 int dcs_lcd_draw_rect_x(const struct DCSLCDScreen *screen,
     int xpos, int ypos, int max_line_len, BaseDisplayItem *item);
 
 int dcs_lcd_draw_text_x(const struct DCSLCDScreen *screen,
-    int xpos, int ypos, int max_line_len, BaseDisplayItem *item);
+    int xpos, int ypos, int max_line_len, BaseDisplayItem *item,
+    BaseDisplayItem items[], size_t items_len, size_t item_index);
 
 int dcs_lcd_draw_scaled_cropped_img_x(const struct DCSLCDScreen *screen,
-    int xpos, int ypos, int max_line_len, BaseDisplayItem *item);
+    int xpos, int ypos, int max_line_len, BaseDisplayItem *item,
+    BaseDisplayItem items[], size_t items_len, size_t item_index);
 
 int dcs_lcd_find_max_line_len(const struct DCSLCDScreen *screen,
     BaseDisplayItem items[], size_t items_len, int xpos, int ypos);
